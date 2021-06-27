ee5288161c518b43cc77582fac4aa91f435c4830
can.liu <553670590@qq.com>
2021-06-27 22:12:56 +0800

    feat:代码

---
+++ src/main/java/com/liucan/kuroky/jvm/MyClassLoader.java
@@ -0,0 +1,53 @@
+package com.liucan.kuroky.jvm;
+
+import java.io.*;
+
+/**
+ * @author liucan
+ */
+public class MyClassLoader extends ClassLoader {
+
+    private final String classpath;
+
+    public MyClassLoader(String classpath) {
+        this.classpath = classpath;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        try {
+            byte[] classDate = getData(name);
+            if (classDate == null) {
+
+            } else {
+                //defineClass方法将字节码转化为类
+                return defineClass(name, classDate, 0, classDate.length);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return super.findClass(name);
+    }
+
+    private byte[] getData(String className) throws IOException {
+        InputStream in = null;
+        ByteArrayOutputStream out = null;
+        String path = classpath + File.separatorChar + className.replace('.', File.separatorChar) + ".class";
+        try {
+            in = new FileInputStream(path);
+            out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[2048];
+            int len = 0;
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+            return out.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            in.close();
+            out.close();
+        }
+        return null;
+    }
+}
