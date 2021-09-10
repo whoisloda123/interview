73116aa7b9b5b2a077e772ebcfede1e0df5fba57
can.liu <553670590@qq.com>
2021-09-10 13:17:53 +0800

    feat:代码

--- src/main/java/com/liucan/kuroky/springclould/Cloud.java
+++ src/main/java/com/liucan/kuroky/springclould/Cloud.java
@@ -1,27 +1,30 @@
 package com.liucan.kuroky.springclould;
 
 /**
+ *
+ * 1.spring如何解决循环依赖的
+ *  https://developer.aliyun.com/article/766880
+ *  a.三级缓存的作⽤：
+ *      第⼀级缓存：存储创建完全成功的单例Bean。
+ *      第三级缓存： 主要设计⽤来解决循环依赖问题的，它是存储只执⾏了实例化步骤的bean（还未依
+ *          赖注⼊和初始化bean操作），但是该缓存的key是beanname， value是ObjectFactory，⽽不是你想存储的bean（将只完成实例化的bean的引⽤交给ObjectFactory持有）。
+ *          ObjectFactory的作⽤：保存提前暴露的Bean的引⽤的同时，针对该Bean进⾏BeanPostProcessor操作，也就是说，在这有⼀个步骤下，可能针对提前暴露的Bean产⽣代理对象。
+ *      第⼆级缓存：主要设计⽤来解决循环依赖时，既有代理对象⼜有⽬标对象的情况下，如何保存代理
+ *          对象。同时还要有⼈保存⽬标对象的引⽤，然后会在最后的部分，使⽤代理对象的引⽤去替换⽬标对象的引⽤。
+ *  b.为何用三级，其实二级也可以
+ *  如果要使用二级缓存解决循环依赖，意味着所有Bean在实例化后就要完成AOP代理，这样违背了Spring设计的原则
+ *  设计之初就是通过AnnotationAwareAspectJAutoProxyCreator这个后置处理器来在Bean生命周期的最后一步来完成AOP代理，而不是在实例化后就立马进行AOP代理。
+ *
+ * 2.微服务注册中心
+ * https://mp.weixin.qq.com/s/YDHk8uwN8VAkt8NXEs4lRg
+ *
+ * 3.could hystrix:https://zhuanlan.zhihu.com/p/150889569
+ *
+ * 4.spring事务传播机制：https://segmentfault.com/a/1190000020386113
+ *
  * @author liucan
  * @date 5/22/21
  */
 public interface Cloud {
-    /* *
-     *
-     *  77.spring如何解决循环依赖的？
-     *  https://blog.csdn.net/lzhcoder/article/details/84144381
-     *      a.场景：
-     *          属性循环依赖：可以解决
-     *          构造器依赖：不能解决
-     *      b.如何检测：
-     *          Bean在创建的时候可以给该Bean打标，如果递归调用回来发现正在创建中的话，即说明了循环依赖了。
-     *      c.属性循环依赖如何解决
-     *          1.单例对象创建过程：实例化->填充属性->初始化
-     *          2.在创建A的时候会把A对应的ObjectFactory放到缓存中(早期引用，只实例化了，但没有填充属性)，在创建A的过程，发现依赖B，然后创建B，发现B
-     *              里面依赖A，这个时候如果发现A正在创建中，会从缓存里面拿到创建好的A（没有填充属性）
-     *          3.实例化完成后，会填充属性，初始化
-     * 2.微服务注册中心
-     * https://mp.weixin.qq.com/s/YDHk8uwN8VAkt8NXEs4lRg
-     *
-     * 3.could hystrix:https://zhuanlan.zhihu.com/p/150889569
-     */
+
 }
