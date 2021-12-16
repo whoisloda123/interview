3febdbad0048c7c7d0041ed067e0947c4c94449a
can.liu <553670590@qq.com>
2021-12-16 23:08:45 +0800

    1221

---
+++ src/main/java/com/liucan/kuroky/other/Xiangmu1.java
@@ -0,0 +1,34 @@
+package com.liucan.kuroky.other;
+
+/**
+ * 项目注意事项：
+ * <ol>
+ *     <li>代码扩展性</li>
+ *     <li>打印日志规范</li>
+ *     <li>代码规范</li>
+ * </ol>
+ *
+ * 个人亮点：
+ * <ol>
+ *     <li>喜欢对之前代码进行优化，如威胁情报</li>
+ *     <li>习惯发现利于和分享对团队效率有提示的工具和方法</li>
+ *     <ul>
+ *         <li>idea 各种插件</li>
+ *         <li>让打包部署更方便，如 jenkins，docker，helm 打包</li>
+ *         <li>部署 api 接口服务</li>
+ *     </ul>
+ *     <li>看源码提升自己的技术，spring 全家桶，mybatis 等</li>
+ *     <li>学习新技术，如 flink</li>
+ * </ol>
+ *
+ * 面试主要事项：
+ * <ol>
+ *     <li>不露痕迹地说出面试官爱听的话</li>
+ *     <li>一定要主动，说出亮点，面试官没有义务挖掘你的亮点</li>
+ *     <li>引导面试官朝你的优势地方去</li>
+ * </ol>
+ * @author liucan
+ * @version 2021/12/16
+ */
+public class Xiangmu1 {
+}
